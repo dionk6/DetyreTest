a4b60d82063179a646e598398a3c4c72f5c100d1
dionk6 <kukadion1@gmail.com>
2021-09-23 17:53:39 +0200

    Add project files.

---
+++ DetyreTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DetyreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
